779bc0b3a9dd9659047bdbb8830c6d4d6452671f
rcrespo9 <rudycrespo@hushmail.com>
2013-11-08 17:17:57 -0500

    added controller, view, and a new attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User
   include Mongoid::Document
   field :age, type: Integer
   field :gender, type: String
+  field :days, type: Integer
   field :remote_addr, type: String
   field :remote_host, type: String
   field :server_name, type: String
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ describe User do
 	before :each do 
 		@valid_age = 20
 		@valid_gender = "Male"
+		@valid_days = 30
 		@valid_remote_addr = "127.0.0.1"
 		@valid_remote_host = "127.0.0.1"
 		@valid_server_name = "localhost"
@@ -12,7 +13,7 @@ describe User do
 		@valid_http_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko)"
 		@valid_http_accept_language =  "en-US,en;q=0.8"
 
-		@user = User.create(age: @valid_age, gender: @valid_gender, remote_addr: @valid_remote_addr, remote_host: @valid_remote_host, server_name: @valid_server_name, server_port: @valid_server_port, http_host: @valid_http_host, http_user_agent: @valid_http_user_agent, http_accept_language: @valid_http_accept_language)
+		@user = User.create(age: @valid_age, gender: @valid_gender, days: @valid_days, remote_addr: @valid_remote_addr, remote_host: @valid_remote_host, server_name: @valid_server_name, server_port: @valid_server_port, http_host: @valid_http_host, http_user_agent: @valid_http_user_agent, http_accept_language: @valid_http_accept_language)
 		
 	end
 
@@ -25,6 +26,12 @@ describe User do
 		describe "User#gender" do 
 			it "should return the correct gender" do 
 				@user.gender.should eq(@valid_gender)
+			end
+		end
+
+		describe "User#days" do 
+			it "should return the correct amount of days" do 
+				@user.days.should eq(@valid_days)
 			end
 		end
 
