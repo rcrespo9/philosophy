e79db16bb685ff1d3c42640e2d113c4c010ccd3f
rcrespo9 <rudycrespo@hushmail.com>
2013-11-10 17:46:37 -0500

    rudimentary form completed

--- app/assets/javascripts/sample.js
+++ app/assets/javascripts/sample.js
@@ -2,19 +2,41 @@
 $(function(){
 	// Initialise noUiSlider
 	$('.noUiSlider').noUiSlider({
-	    range: [0, 2001],
-	    start: 0,
+	    range: ["0", "2001"],
+	    start: "0",
 	    handles: 1,
 	    slide: function(){
 			var days = parseInt($(this).val());
+			var money = "$" + parseInt($(this).val()) * 500;
 
-			if (days == 2001) {
-				days = 2000 + "+"
+			if (days == "2001" && money > "$1000000") {
+				days = "+2000"
+				money = "+$1000000"
 			};
 			
-			$(this).next('h1').text(
-				days + ' days sold'
-			);					
-	   }
+			$(this).next('#days').text(
+				  days + ' days sold'
+			);
+
+			$(this).nextAll('#money').text(
+				  money + ' earned'
+			);				
+	    }
+
 	});
+
+	$('#submit_value').on("click", function() {
+		var days = parseInt($('.noUiSlider').val());
+			if (days == "2001") {
+				days = "+2000"
+			};
+		alert(days);
+
+		var age = $('#age option:selected').text();
+		alert(age);
+
+		var gender = $('input[name=gender]:checked').val();
+		alert(gender);
+	});
+
 });
