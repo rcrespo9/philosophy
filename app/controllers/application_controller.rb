1399311860b292c0727aa5280c379ead8c5080d6
Joe21 <joej21@gmail.com>
2013-11-11 16:55:45 -0500

    commented out the cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :cookie_checker
+  # before_filter :cookie_checker
 
-  def cookie_checker
-  	if cookie exists
-  		what happens?
-  	else
-  		cookie doesnt exist create cookie
-  	end
-  end
+  # def cookie_checker
+  # 	if cookie exists
+  # 		direct to results
+  # 	else
+  # 		cookie doesnt exist create cookie
+  # 		display vote page
+  # 		(make exception for before_filter for the results url)
+  # 	end
+  # end
 end
