39cd027fc8bada96156060ac0cf7bf702145ef37
Joe21 <joej21@gmail.com>
2013-11-11 15:53:01 -0500

    beginning cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :cookie_checker
+
+  def cookie_checker
+  	if cookie exists
+  		what happens?
+  	else
+  		cookie doesnt exist create cookie
+  	end
+  end
 end
