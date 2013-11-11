4c54f7ee68b6ade3cefab83bf145fc65e3150a32
rcrespo9 <rudycrespo@hushmail.com>
2013-11-11 15:07:29 -0500

    horizontal  parallax working

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,6 +1,81 @@
+var rightKey, leftKey;
 
 // Wait until the document is ready
 $(function(){
+
+	//Set up the triggers for the arrow keys
+	$(document).keydown(function(e){
+		if (e.keyCode == 37 && typeof leftKey === 'function') {
+			leftKey();
+		} else if(e.keyCode == 39 && typeof rightKey === 'function') {
+			rightKey();
+		}
+	});
+
+	parallax.add($("#selector"))
+			.add($("#home"));
+
+	parallax.background = $("body");
+
+	//Clears each page navigation on load
+	parallax.preload = function(){
+		rightKey = leftKey = "";
+		$(".control").hide();
+	};
+
+
+	//Setting up page navigation
+	parallax.home.onload=function(){
+		setRight("selector", "Selector");
+	};
+
+	parallax.selector.onload=function(){
+		setLeft("home", "Home");
+	};
+
+
+	//Sets the correct triggers for the arrows, plus arrow keys
+	function setRight(page, text){
+		$("#rightText").text(text);
+		$("#rightControl").show().unbind('click').click(function(){
+			parallax[page].right();
+		});
+		rightKey = function(){
+			parallax[page].right();
+		};
+	}
+
+	function setLeft(page, text){
+		$("#leftText").text(text);
+		$("#leftControl").show().unbind('click').click(function(){
+			parallax[page].left();
+		});
+		leftKey = function(){
+			parallax[page].left();
+		};
+	}
+
+
+	//The fadey bits
+	$("#leftControl").mouseenter(function(){
+		$("#leftArrow").fadeTo(500,1);
+		$("#leftText").fadeTo(500,1);
+	}).mouseleave(function(){
+		$("#leftArrow").stop().fadeTo(500, 0.2);
+		$("#leftText").stop().fadeTo(500,0);
+	});
+
+	$("#rightControl").mouseenter(function(){
+		$("#rightArrow").fadeTo(500,1);
+		$("#rightText").fadeTo(500,1);
+	}).mouseleave(function(){
+		$("#rightArrow").stop().fadeTo(500, 0.2);
+		$("#rightText").stop().fadeTo(500,0);
+	});
+
+	$(".control").hide();
+	parallax.home.show();
+
 	// Initialise noUiSlider
 	$('.noUiSlider').noUiSlider({
 		range: ["0", "2001"],
